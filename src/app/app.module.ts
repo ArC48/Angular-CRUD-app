8f5b63cee6f0dca7dea3896f15bac69c00cbc9d3
ArC48 <beqa3352@gmail.com>
2022-04-27 20:23:17 +0400

    added json server and additional styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeRegisterComponent } from './employee-register/employee-register.component';
 import { EmployeeComponent } from './employee-register/employee/employee.component';
+import {NgxPaginationModule} from 'ngx-pagination';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { MatNativeDateModule } from '@angular/material/core';
+import { IntercaptorService } from './shared/intercaptor.service';
 
 @NgModule({
   declarations: [
@@ -14,9 +20,17 @@ import { EmployeeComponent } from './employee-register/employee/employee.compone
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgxPaginationModule,
+    BrowserAnimationsModule,
+    MatProgressBarModule,
+    MatNativeDateModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: IntercaptorService, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
